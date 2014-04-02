dba43f6c70a10342665a214b7845a2eac7ea5da7
LucasRPC <lucas.rodriguez-pena@hotmail.com>
2014-04-02 00:14:23 -0400

    Update BioZed 0.1.lua
    Lag free circles

--- BioZed 0.1.lua
+++ BioZed 0.1.lua
@@ -117,6 +117,35 @@ function autoIgnite()
                 end
         end
 end
+
+--Lagfree Circles by barasia, vadash and viseversa
+function DrawCircleNextLvl(x, y, z, radius, width, color, chordlength)
+    radius = radius or 300
+		quality = math.max(8,round(180/math.deg((math.asin((chordlength/(2*radius)))))))
+		quality = 2 * math.pi / quality
+		radius = radius*.92
+    local points = {}
+    for theta = 0, 2 * math.pi + quality, quality do
+        local c = WorldToScreen(D3DXVECTOR3(x + radius * math.cos(theta), y, z - radius * math.sin(theta)))
+        points[#points + 1] = D3DXVECTOR2(c.x, c.y)
+    end
+    DrawLines2(points, width or 1, color or 4294967295)
+end
+
+function round(num) 
+	if num >= 0 then return math.floor(num+.5) else return math.ceil(num-.5) end
+end
+
+function DrawCircle2(x, y, z, radius, color)
+    local vPos1 = Vector(x, y, z)
+    local vPos2 = Vector(cameraPos.x, cameraPos.y, cameraPos.z)
+    local tPos = vPos1 - (vPos1 - vPos2):normalized() * radius
+    local sPos = WorldToScreen(D3DXVECTOR3(tPos.x, tPos.y, tPos.z))
+    if OnScreen({ x = sPos.x, y = sPos.y }, { x = sPos.x, y = sPos.y }) then
+        DrawCircleNextLvl(x, y, z, radius, 1, color, 75)	
+    end
+end
+
  
 function OnDraw()
  if Config.Qdraw then
