6251868a700f693b8197f69a10e4d38545da4456
LucasRPC <lucas.rodriguez-pena@hotmail.com>
2014-07-23 23:44:38 +0100

    Update BioZed.lua

--- BioZed.lua
+++ BioZed.lua
@@ -1,6 +1,6 @@
 if myHero.charName ~= "Zed" then return end
 if VIP_USER then
-    PrintChat("<font color=\"#FF0000\" >> BioZed By Lucas and Pyryoer v 1.91 <</font> ")
+    PrintChat("<font color=\"#FF0000\" >> BioZed By Lucas v 1.91 <</font> ")
 end
 
 local RREADY, QREADY, WREADY, EREADY
